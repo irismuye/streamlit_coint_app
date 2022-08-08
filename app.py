081fe57bef3c596583e752d96057a27de1b00aad
irismguo <110634890+irismguo@users.noreply.github.com>
2022-08-08 15:17:24 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import datetime
 import pandas as pd
 import requests
 import json
-import pytz
+import webbrowser
 
 from binance.um_futures import UMFutures
 
@@ -17,16 +17,26 @@ st.set_page_config(
     layout="wide",
 )
 
+
 @st.experimental_memo
 def exchange_info():
     client = UMFutures()
     exchange_info = client.exchange_info()
-#     exchange_info = requests.get('https://api.binance.com/fapi/v1/exchangeInfo').json()
+    #     exchange_info = requests.get('https://api.binance.com/fapi/v1/exchangeInfo').json()
     symbols = exchange_info["symbols"]
 
-    return symbols
+    perps = []
+    for symbol in symbols:
+        if symbol['contractType'] == 'PERPETUAL' and symbol['quoteAsset'] == 'USDT' and symbol["status"] == "TRADING":
+            perps.append(symbol['symbol'])
 
-symbols = exchange_info()
+    return perps
+
+
+perps = exchange_info()
+global df1, df5
+
+st.session_state.load = True
 root_url = 'https://api.binance.com/api/v1/klines'
 time_url = "https://api.binance.com/api/v3/time"
 
@@ -37,7 +47,7 @@ url = 'https://api.binance.com/api/v3/ticker?symbols=%5B%22BTCUSDT%22,%22ETHUSDT
 # read csv from a URL
 
 def get_bars(symbol, interval='1m'):
-    url = root_url + '?symbol=' + symbol + '&interval=' + interval + '&limit='  + str(1000)
+    url = root_url + '?symbol=' + symbol + '&interval=' + interval + '&limit=' + str(1000)
     data = json.loads(requests.get(url).text)
     df = pd.DataFrame(data)
     df.columns = ['open_time',
@@ -47,12 +57,9 @@ def get_bars(symbol, interval='1m'):
     df.index = [datetime.datetime.fromtimestamp(x / 1000.0) for x in df.close_time]
     return df
 
+
 # @st.cache(allow_output_mutation=True)
-def combine(interval, rank = 10):
-    perps = []
-    for symbol in symbols:
-        if symbol['contractType'] == 'PERPETUAL' and symbol['quoteAsset'] == 'USDT' and symbol["status"] == "TRADING":
-            perps.append(symbol['symbol'])
+def combine(interval, rank=10):
 
     data = {}
     volume = {}
@@ -76,30 +83,23 @@ def combine(interval, rank = 10):
     return close_df, selected
 
 
-
-
 # dashboard title
 st.title("Real-Time / Live Crypto Dashboard")
-
 # top-level filters
-_rank = st.selectbox("Select the Volume Rank", range(10, 143))
+
+
 
 # creating a single-element container
 placeholder = st.empty()
 place = st.empty()
 
-def convert_df(df):
-     # IMPORTANT: Cache the conversion to prevent computation on every rerun
-     return df.to_excel().encode('utf-8')
 
-
+st.session_state.run = 0
 
 def price():
-
     price = requests.get(url).json()
 
-
-        # create three columns
+    # create three columns
     with placeholder.container():
         kpi1, kpi2, kpi3 = st.columns(3)
 
@@ -126,61 +126,103 @@ def price():
 
         st.markdown('Price at Server Time {}'.format(now_time.strftime("%Y-%m-%d %H:%M:%S")))
 
+st.session_state.fig = [0, 0]
 
 def refresh(rank):
+    # webbrowser.open("http://www.github.com")
+    global df1, df5
     df1 = combine('1m', rank)
     df5 = combine('5m', rank)
+
     # st.balloons()
     st.session_state.load = True
+    st.session_state.data = [df1, df5]
 
-    with place.container():
-        fig_col1, fig_col2 = st.columns(2)
+    st.session_state.fig[0] = plot_coint(df1[0], '1m')
+    st.session_state.fig[1] = plot_coint(df1[0], '5m')
 
-        with fig_col1:
-            st.markdown("### 1 Minute")
-            fig = plot_coint(df1[0], '1m')
-            st.markdown('Close Price at Server Time {}'.format(df1[0].index[-1].strftime("%Y-%m-%d %H:%M:%S")))
-            st.write(fig)
+    if st.session_state.load:
+        with place.container():
+            fig_col1, fig_col2 = st.columns(2)
 
-            f1 = st.selectbox('Select First Future:', df1[1])
-            f2 = st.selectbox('Select Second Future:', df1[1])
+            with fig_col1:
+                st.markdown("### 1 Minute")
+                st.markdown('Close Price at Server Time {}'.format(df1[0].index[-1].strftime("%Y-%m-%d %H:%M:%S")))
+                if st.session_state.fig[0] != 0:
+                    st.write(st.session_state.fig[0])
 
+            with fig_col2:
 
-            # csv = convert_df(df1[0][[f1, f2]])
+                if st.session_state.load:
+                    st.markdown("### 5 Minute")
 
-            # st.download_button(
-            #     label="Download data as EXCEL",
-            #     data=csv,
-            #     file_name='{}_{}.xlsx'.format(f1, f2),
-            #     mime='text/xlsx',
-            #     )
+                    st.markdown('Close Price at Server Time {}'.format(df5[0].index[-1].strftime("%Y-%m-%d %H:%M:%S")))
+                # st.markdown("###### Updated {}".format(datetime.datetime.now()))
+                    if st.session_state.fig[1] != 0:
+                        st.write(st.session_state.fig[1])
 
-        with fig_col2:
-            st.markdown("### 5 Minute")
-            fig2 = plot_coint(df5[0], '5m')
-
-            st.markdown('Close Price at Server Time {}'.format(df5[0].index[-1].strftime("%Y-%m-%d %H:%M:%S")))
-            # st.markdown("###### Updated {}".format(datetime.datetime.now()))
-            st.write(fig2)
-
-            st.selectbox('Select First Future:', df5[1])
-            st.selectbox('Select Second Future:', df5[1])
 
             # csv = convert_df(df5[0][[f1, f2]])
 
-            connect = st.button("Connect")
+def freeze():
+    if len(f1) >= 2:
+        st.session_state.data[0][0][f1].to_csv('one_min.csv')
+    if len(f5) >= 2:
+        st.session_state.data[1][0][f5].to_csv('five_min.csv')
+    st.write('HEY!!!!!')
 
-            if connect:
+st.sidebar.title("Crypto Cointegration Analysis")
+with st.sidebar:
 
-                st.session_state.close_price = df5[0][f1, f2]
-
-        # st.markdown("### Detailed Data View")
-        # st.dataframe(df)
-        # time.sleep(1)
-        
-st.session_state.load = True
-while True:
     if st.session_state.load:
-        price()
-        refresh(int(_rank))
-    # st.markdown("###### Updated {}".format(datetime.datetime.now()))
+        _rank = st.selectbox("Select the Volume Rank", range(10, 143))
+        c1, c2 = st.columns(2)
+        with c1:
+            run_button = st.button("RUN")
+
+        if st.session_state.run >= 1:
+            with placeholder.container():
+                fig_col1, fig_col2 = st.columns(2)
+                with fig_col1:
+                    st.markdown("### 1 Minute")
+                    st.markdown('Close Price at Server Time {}'.format(
+                        st.session_state.data[0][0].index[-1].strftime("%Y-%m-%d %H:%M:%S")))
+                    if st.session_state.fig[0] != 0:
+                        st.write(st.session_state.fig[0])
+
+                with fig_col2:
+
+                    if st.session_state.load:
+                        st.markdown("### 5 Minute")
+
+                        st.markdown('Close Price at Server Time {}'.format(
+                            st.session_state.data[1][0].index[-1].strftime("%Y-%m-%d %H:%M:%S")))
+                        # st.markdown("###### Updated {}".format(datetime.datetime.now()))
+                        if st.session_state.fig[1] != 0:
+                            st.write(st.session_state.fig[1])
+
+        if run_button:
+            with st.spinner('Running...'):
+                refresh(int(_rank))
+                st.session_state.run += 1
+
+        with c2:
+            st.button("FREEZE", on_click=freeze)
+
+
+        with st.form(key = "pairs"):
+
+            f1 = st.multiselect('Select Two Futures for 1-min Data:', perps)
+            f5 = st.multiselect('Select Two Futures for 5-min Data:', perps)
+
+            submit = st.form_submit_button(label='FREEZE', on_click=freeze)
+
+
+        link = '[GitHub](http://www.github.com)'
+        st.markdown(link, unsafe_allow_html=True)
+
+
+
+
+while True:
+    price()
